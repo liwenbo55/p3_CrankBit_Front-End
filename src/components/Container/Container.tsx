cd6ed9f629a5361d4f5ea149300f6676eca97597
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-06-08 21:25:47 +1000

    refo: tailwind config

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,27 +5,16 @@ export enum Size {
   Default,
 }
 
-export enum Space {
-  Default,
-}
-
 interface Props {
   children: ReactNode
   className?: string
   size?: Size
-  space?: Space
 }
 
-const Container: FC<Props> = ({
-  size = Size.Default,
-  space = Space.Default,
-  children,
-  className = undefined,
-}) => (
+const Container: FC<Props> = ({ size = Size.Default, children, className = undefined }) => (
   <div
     className={classNames(
-      size === Size.Default && 'min-w-[390px] md:max-w-container',
-      space === Space.Default && ['px-6', 'md:px-9'],
+      size === Size.Default && 'min-w-[390px] max-w-container',
       'mx-auto',
       className
     )}

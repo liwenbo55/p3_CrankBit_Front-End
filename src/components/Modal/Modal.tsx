6dc6b3192ee46caa7c4daf5e881da0b5edc9c43e
Alan <68600416+Qiming-Liu@users.noreply.github.com>
2023-06-28 22:54:48 +0930

    fix: lint (#65)
    * fix
    
    * fix
    
    * test

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,20 +3,18 @@ import CheckRounded from '@mui/icons-material/CheckRounded'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 
 interface Props {
-  onClose: ()=> void
+  onClose: () => void
 }
-const Modal: FC<Props> = ({onClose}) =>
-(<div className="fixed top-0 left-0 bottom-0 right-0 bg-background/40 flex items-center justify-center z-50">
-        <div className="rounded-[10px] w-[494px] h-[405px] pt-5 text-center bg-white shadow-md border border-background/40">
-             <div className="text-end">
-              <CloseRoundedIcon onClick={onClose} className="cursor-pointer mb-10 mr-5" />
-             </div>
-              <CheckRounded className="text-white bg-[#0EA739] w-[99px] h-[99px] rounded-full" />
-          <div className="font-semi-bold text-xl mt-12">
-           Sign Up Successfully
-          </div>
-        </div>
+const Modal: FC<Props> = ({ onClose }) => (
+  <div className="fixed top-0 left-0 bottom-0 right-0 bg-background/40 flex items-center justify-center z-50">
+    <div className="rounded-[10px] w-[494px] h-[405px] pt-5 text-center bg-white shadow-md border border-background/40">
+      <div className="text-end">
+        <CloseRoundedIcon onClick={onClose} className="cursor-pointer mb-10 mr-5" />
       </div>
-  )
+      <CheckRounded className="text-white bg-[#0EA739] w-[99px] h-[99px] rounded-full" />
+      <div className="font-semi-bold text-xl mt-12">Sign Up Successfully</div>
+    </div>
+  </div>
+)
 
 export default Modal

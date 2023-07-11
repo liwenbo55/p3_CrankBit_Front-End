62cdc55c90fee439b347b99dc63196ddc83e1b73
TaoGit2023 <128274932+TaoGit2023@users.noreply.github.com>
2023-07-11 22:37:45 +1000

    Cb 137 modify myaccount page (#97)
    * modify account page
    
    * account page
    
    ---------
    
    Co-authored-by: SteveGuoh <127906105+SteveGuoh@users.noreply.github.com>

--- src/pages/MyAccount/MyAccount.tsx
+++ src/pages/MyAccount/MyAccount.tsx
@@ -4,16 +4,29 @@ import CompanyLayout from '@/layouts/UserLayout/UserLayout'
 
 const MyAccount: FC = () => (
   <CompanyLayout>
-    <div className="bg-userContent min-h-screen p-10">
-      <h1>Hi, Alonso</h1>
-      <div className="text-end">Edit</div>
-      <div className="flex justify-around">
-        <div> email</div> <div>123.com</div>
+    <div className="bg-userContent min-h-screen p-10 ">
+      <h1 className="mb-4">Hi, Tao</h1>
+      <div className="bg-white rounded-lg p-10">
+        <div>
+          <p className="inline-block">Name:</p>
+          <p className="inline-block ml-20">Tao</p>
+        </div>
+        <div>
+          <p className="inline-block">Email:</p>
+          <p className="inline-block ml-20">hello@123.com</p>
+        </div>
+        <div>
+          <p className="inline-block">DOB:</p>
+          <p className="inline-block ml-20">08/09/1999</p>
+        </div>
       </div>
-      <div>
-        <div className="text-end">Edit</div>
-        <h1>companylist</h1>
-        <div className="flex gap-10">
+      <div className="mt-10">
+        <div className='flex space-around bg-gray p-8'>
+          <h2 className="text-bold text-lg">CompanyList</h2>
+          <button type='button' className='bg-black w-[200px] h-[30px] text-white text-center ml-[500px] rounded-lg shadow-mb'>Add New Company</button>
+        </div>
+
+        <div className="flex gap-10 bg-white shadow-md rounded-lg p-20">
           <Link to="/user/my-reports">
             <img src="./svg/CompanyOne.png" alt="Company Logo" className="h-[100px] rounded-[100px]" />
           </Link>

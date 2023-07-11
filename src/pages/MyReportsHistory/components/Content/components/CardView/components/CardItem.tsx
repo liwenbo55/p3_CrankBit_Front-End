df9110891bf8d40ce757b75a0f13bffbd40c63e8
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-07-11 11:55:16 +1000

    userContent

--- src/pages/MyReportsHistory/components/Content/components/CardView/components/CardItem.tsx
+++ src/pages/MyReportsHistory/components/Content/components/CardView/components/CardItem.tsx
@@ -8,8 +8,8 @@ interface Props {
 
 const CardItem: FC<Props> = ({ report, lastUpdated, taskNumber }) => (
   <div>
-    <div className="relative p-5 rounded bg-userContent h-[190px] w-[250px] border border-black shadow-lg">
-      <div className="absolute top-0 right-0 w-10 h-10 bg-white border-b border-black transform rotate-45 translate-x-5 translate-y-[-20px]" />
+    <div className="relative p-5 rounded bg-white h-[190px] w-[250px] border border-black shadow-lg">
+      <div className="absolute top-0 right-0 w-10 h-10 bg-userContent border-b border-black transform rotate-45 translate-x-5 translate-y-[-20px]" />
       <div className="absolute top-0 right-0 w-7 h-7 border-b border-l border-black transform" />
       <div className="grid gap-4">
         <div>

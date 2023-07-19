563f85202a4748219b86004349a324c2f77b92cc
SteveGuoh <127906105+SteveGuoh@users.noreply.github.com>
2023-07-19 08:36:14 +0800

    bug fixed (#103)

--- src/pages/Home/components/Pricing/Pricing.tsx
+++ src/pages/Home/components/Pricing/Pricing.tsx
@@ -3,7 +3,7 @@ import DisplayPlans from './components/DisplayPlans/DisplayPlans'
 
 const Pricing = forwardRef<HTMLDivElement>((props, ref) => (
   <div className="mx-auto text-center my-32" ref={ref}>
-    <h2 className="text-2xl font-bold mb-6">Pricing</h2>
+    <h2 className="text-2xl text-white font-bold mb-6">Pricing</h2>
     <DisplayPlans />
   </div>
 ))
